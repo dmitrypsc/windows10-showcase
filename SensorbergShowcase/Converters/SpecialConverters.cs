209937d75d790a21a22ea3cf4532b0b4d039d4ee
Tomi Paananen <tomi.paananen@kolumbus.fi>
2016-03-08 12:58:45 +0200

    Initial commit of Sensorberg Showcase app to its new repository.
    Changes compared to the previous version:
    * Misbehaving QR code scanner removed until a new, better one is implemented.
    * Visual assets updated.
    * Added a beacon count indicator to the scanner header.
    * Strings partially localized. Some still left to do.
    * Toggle scanner button behavior improved.

---
+++ SensorbergShowcase/Converters/SpecialConverters.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+
+namespace SensorbergShowcase.Converters
+{
+    public class BoolToScannerStateTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool valueAsBool = (value is bool && (bool)value);
+            return valueAsBool ? "Scanning..." : "Scanner is stopped";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BoolToAdvertisingButtonTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool valueAsBool = (value is bool && (bool)value);
+            return valueAsBool ? "stop advertising" : "start advertising";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BoolToBackgroundImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool valueAsBool = (value is bool && (bool)value);
+            return valueAsBool ? "ms-appx:///Assets/Graphics/BigBackground.jpg" : "ms-appx:///Assets/Graphics/Background.jpg";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
